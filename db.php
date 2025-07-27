6a9fc0694e283a5a7b2519f1b254f0dede7ec643
Thomasshelby786 <skowaisali358@gmail.com>
2025-07-27 22:48:03 +0530

    Update db.php

--- db.php
+++ db.php
@@ -1,10 +1,12 @@
 <?php
-$servername = "sql12.freesqldatabase.com"
-$username = "sql12792201"
-$password = "KRl28aDYUU"
-$dbname = "sql12792201"
+$servername = "sql12.freesqldatabase.com";
+$username = "sql12792201";
+$password = "KRl28aDYUU";   
+$dbname = "sql12792201";
+$port = 3306;
 
-$conn = mysqli_connect($servername, $username, $password, $dbname);
+$conn = mysqli_connect($servername, $username, $password, $dbname, $port);
+
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
